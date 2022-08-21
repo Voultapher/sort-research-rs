15e5b201f503113c3483b92388090fa4d3e0e36f
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2022-08-21 17:40:07 +0200

    Produce overview graphs

--- util/graph_bench_result/produce_graphs.py
+++ util/graph_bench_result/produce_graphs.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import statistics
 
 from bokeh import models
 
@@ -8,7 +9,7 @@ from bokeh.plotting import figure, gridplot, ColumnDataSource
 from bokeh.resources import CDN
 from bokeh.embed import file_html
 
-TRANSFORMS = ["i32"]  # , "u64", "string", "1k", "f128"]
+TRANSFORMS = ["i32", "u64", "string", "1k", "f128"]
 
 
 class BenchEntry:
@@ -48,7 +49,7 @@ def extract_groups(bench_result):
 TOOLS = None
 
 
-def init_tools():
+def init_tools_detailed():
     global TOOLS
     TOOLS = [
         models.WheelZoomTool(),
@@ -59,6 +60,23 @@ def init_tools():
                 ("Name", "@name"),
                 ("Test Size", "@x"),
                 ("Time in ns", "@y"),
+            ],
+        ),
+        models.ResetTool(),
+    ]
+
+
+def init_tools_overview():
+    global TOOLS
+    TOOLS = [
+        models.WheelZoomTool(),
+        models.BoxZoomTool(),
+        models.PanTool(),
+        models.HoverTool(
+            tooltips=[
+                ("Name", "@name"),
+                ("Test Size", "@x"),
+                ("Relative speedup", "@y%"),
             ],
         ),
         models.ResetTool(),
@@ -135,24 +153,118 @@ def plot_detailed(name_a, groups_a, name_b, groups_b):
     for transform in TRANSFORMS:
         for temp in ["hot", "cold"]:
             filter_name = f"{temp}-{transform}-"
-            init_tools()
-            grid_plot = gridplot(
-                [
-                    make_detail_plot(
-                        bench_name,
-                        name_a,
-                        group_a,
-                        name_b,
-                        groups_b[bench_name],
-                    )
-                    for bench_name, group_a in groups_a.items()
-                    if bench_name.startswith(filter_name)
-                ]
-            )
+            init_tools_detailed()
+            detail_plots = [
+                make_detail_plot(
+                    bench_name,
+                    name_a,
+                    group_a,
+                    name_b,
+                    groups_b[bench_name],
+                )
+                for bench_name, group_a in groups_a.items()
+                if bench_name.startswith(filter_name)
+            ]
 
+            grid_plot = gridplot(detail_plots)
             html = file_html(grid_plot, CDN, transform)
             with open(f"{temp}-{transform}.html", "w+") as outfile:
                 outfile.write(html)
+
+
+# If time_a is faster than time_b -> % faster than time_b
+# If time_b is faster than time_a -> % faster than time_a as negative number
+# 100 == time_a 2x faster than time_b
+# -100 == time_b 2x faster than time_a
+def relative_speedup(time_a, time_b):
+    if time_a <= time_b:
+        # time_a is faster.
+        return ((time_b / time_a) - 1) * 100
+    else:
+        # time_b is faster
+        return -(((time_a / time_b) - 1) * 100)
+
+
+def plot_distribution(temp, transform, name_a, groups_a, name_b, groups_b):
+    plot = figure(
+        title=f"{name_a} vs {name_b} ({temp}-{transform})",
+        x_axis_label="Input Size (log)",
+        x_axis_type="log",
+        y_axis_label=f"Relative speedup in %. {name_a} > 0 > {name_b}. 100% == 2x",
+        tools="",
+    )
+    add_tools_to_plot(plot)
+
+    test_sizes = []
+    speedups = []
+    names = []
+
+    filter_name = f"{temp}-{transform}-"
+    for bench_name, group_a in groups_a.items():
+        if filter_name not in bench_name:
+            continue
+
+        group_b = groups_b[bench_name]
+
+        for entry_name, entry_a in group_a.items():
+            entry_b = group_b[entry_name]
+            assert entry_a.size == entry_b.size
+
+            test_sizes.append(entry_a.size)
+            speedups.append(relative_speedup(entry_a.time_ns, entry_b.time_ns))
+            names.append(entry_name)
+
+    data = {"x": test_sizes, "y": speedups, "name": names}
+    source = ColumnDataSource(data=data)
+
+    plot.scatter(
+        source=source,
+        line_alpha=0.35,
+        fill_alpha=0.18,
+        line_color="royalblue",
+        fill_color="royalblue",
+        size=10,
+    )
+
+    test_sizes_unique = list(set(test_sizes))
+    test_sizes_unique.sort()
+
+    # This is really inefficient, but its ok for producing graphs.
+    speedups_random_median = [
+        [
+            s
+            for t, s, n in zip(test_sizes, speedups, names)
+            if t == test_size and "-random-" in n
+        ][0]
+        for test_size in test_sizes_unique
+    ]
+
+    plot.line(
+        x=test_sizes_unique,
+        y=speedups_random_median,
+        color="orange",
+        legend_label="random pattern",
+    )
+
+    plot.legend.location = "top_left"
+
+    return plot
+
+
+def plot_overview(name_a, groups_a, name_b, groups_b):
+    from bokeh.plotting import show
+
+    for temp in ["hot", "cold"]:
+        init_tools_overview()
+        distribution_plots = [
+            plot_distribution(
+                temp, transform, name_a, groups_a, name_b, groups_b
+            )
+            for transform in TRANSFORMS
+        ]
+
+        grid_plot = gridplot(distribution_plots, ncols=3)
+        show(grid_plot)
 
 
 if __name__ == "__main__":
@@ -165,4 +277,9 @@ if __name__ == "__main__":
     groups_a = extract_groups(result_a)
     groups_b = extract_groups(result_b)
 
-    plot_detailed(name_a, groups_a, name_b, groups_b)
+    assert len(groups_a) == len(groups_b)
+    for bench_name, group_a in groups_a.items():
+        assert len(group_a) == len(groups_b[bench_name])
+
+    # plot_detailed(name_a, groups_a, name_b, groups_b)
+    plot_overview(name_a, groups_a, name_b, groups_b)
