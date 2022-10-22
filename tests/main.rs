e816cd9603e015e6dba479ce3479d887aaa8bd49
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2022-10-22 12:07:39 +0200

    Use FnMut instead of statics in tests

--- tests/main.rs
+++ tests/main.rs
@@ -415,28 +415,26 @@ fn observable_is_less() {
             .map(|val| CompCount::new(val))
             .collect::<Vec<_>>();
 
-        use std::sync::atomic::{AtomicU32, Ordering};
-        static COMP_COUNT_GLOBAL: AtomicU32 = AtomicU32::new(0);
-
-        COMP_COUNT_GLOBAL.store(0, Ordering::SeqCst);
+        let mut comp_count_gloabl = 0;
 
         test_sort::sort_by(&mut test_input, |a, b| {
             a.comp_count.replace(a.comp_count.get() + 1);
             b.comp_count.replace(b.comp_count.get() + 1);
-            COMP_COUNT_GLOBAL.fetch_add(1, Ordering::SeqCst);
+            comp_count_gloabl += 1;
 
             a.val.cmp(&b.val)
         });
 
         let total_inner: u32 = test_input.iter().map(|c| c.comp_count.get()).sum();
-        let total_global = COMP_COUNT_GLOBAL.load(Ordering::SeqCst);
 
-        assert_eq!(total_inner, total_global * 2);
+        assert_eq!(total_inner, comp_count_gloabl * 2);
     };
 
     test_fn(patterns::ascending(10));
+    test_fn(patterns::ascending(19));
     test_fn(patterns::random(12));
-    // test_fn(patterns::random(TEST_SIZES[TEST_SIZES.len() - 1]));
+    test_fn(patterns::random(20));
+    test_fn(patterns::random(TEST_SIZES[TEST_SIZES.len() - 1]));
 }
 
 fn calc_comps_required(test_data: &[i32]) -> u32 {
@@ -500,20 +498,24 @@ fn violate_ord_retain_original_set() {
 
     // Make sure we get a good distribution of random orderings, that are repeatable with the seed.
     // Just using random_uniform with the same size and range will always yield the same value.
-    let random_orderings = patterns::random_uniform(100_000, 0..2);
-    use std::sync::atomic::{self, AtomicI32, AtomicU32};
+    let random_orderings = patterns::random_uniform(5_000, 0..2);
+
+    let mut random_idx: u32 = 0;
+
+    let mut last_element_a = -1;
+    let mut last_element_b = -1;
 
     // Examples, a = 3, b = 5, c = 9.
     // Correct Ord -> 10010 | is_less(a, b) is_less(a, a) is_less(b, a) is_less(a, c) is_less(c, a)
-    let invalid_ord_comp_functions: Vec<Box<dyn Fn(&i32, &i32) -> Ordering>> = vec![
+    let mut invalid_ord_comp_functions: Vec<Box<dyn FnMut(&i32, &i32) -> Ordering>> = vec![
         Box::new(|_a, _b| -> Ordering {
             // random
             // Eg. is_less(3, 5) == true, is_less(3, 5) == false
 
-            static RANDOM_IDX: AtomicU32 = AtomicU32::new(0);
-            let ridx = RANDOM_IDX.fetch_add(1, atomic::Ordering::SeqCst) as usize;
+            let ridx = random_idx as usize;
+            random_idx += 1;
             if ridx + 1 == random_orderings.len() {
-                RANDOM_IDX.store(0, atomic::Ordering::SeqCst);
+                random_idx = 0;
             }
 
             let idx = random_orderings[ridx] as usize;
@@ -542,16 +544,13 @@ fn violate_ord_retain_original_set() {
         }),
         Box::new(|a, b| -> Ordering {
             // Transitive breaker. remember last element -> 10001
-            static LAST_ELEMENT_A: AtomicI32 = AtomicI32::new(-1);
-            static LAST_ELEMENT_B: AtomicI32 = AtomicI32::new(-1);
+            let lea = last_element_a;
+            let leb = last_element_b;
 
-            let last_element_a = LAST_ELEMENT_A.load(atomic::Ordering::SeqCst);
-            let last_element_b = LAST_ELEMENT_B.load(atomic::Ordering::SeqCst);
+            last_element_a = *a;
+            last_element_b = *b;
 
-            LAST_ELEMENT_A.store(*a, atomic::Ordering::SeqCst);
-            LAST_ELEMENT_B.store(*b, atomic::Ordering::SeqCst);
-
-            if *a == last_element_a && *b != last_element_b {
+            if *a == lea && *b != leb {
                 b.cmp(a)
             } else {
                 a.cmp(b)
@@ -559,13 +558,13 @@ fn violate_ord_retain_original_set() {
         }),
     ];
 
-    for comp_func in invalid_ord_comp_functions {
+    for comp_func in &mut invalid_ord_comp_functions {
         // Larger sizes may take very long so filter them out here.
-        for test_size in &TEST_SIZES[0..TEST_SIZES.len() - 3] {
+        for test_size in &TEST_SIZES[0..TEST_SIZES.len() - 2] {
             let mut test_data = patterns::random(*test_size);
             let sum_before: i64 = test_data.iter().map(|x| *x as i64).sum();
 
-            test_sort::sort_by(&mut test_data, &comp_func);
+            test_sort::sort_by(&mut test_data, &mut *comp_func);
 
             // If the sum before and after don't match, it means the set of elements hasn't remained the
             // same.
