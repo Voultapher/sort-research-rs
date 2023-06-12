a1648382b0c946e1ef9dbec46551007386d0d25a
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2023-06-12 08:23:12 +0200

    Fix default test impl

--- tests/main.rs
+++ tests/main.rs
@@ -1,5 +1,5 @@
 use sort_test_tools::instantiate_sort_tests;
 
-type TestSort = sort_comp::other::singeli_singelisort::SortImpl;
+type TestSort = sort_comp::unstable::rust_ipnsort::SortImpl;
 
 instantiate_sort_tests!(TestSort);
