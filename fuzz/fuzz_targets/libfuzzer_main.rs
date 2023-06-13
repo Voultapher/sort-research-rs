c9e0be1b2c00b15a6abab9b9be3cedc6c6996e4a
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2023-06-13 18:15:17 +0200

    Set default fuzz target to ipnsort

--- fuzz/fuzz_targets/libfuzzer_main.rs
+++ fuzz/fuzz_targets/libfuzzer_main.rs
@@ -2,7 +2,7 @@
 
 use libfuzzer_sys::fuzz_target;
 
-use sort_comp::stable::rust_ipn as test_sort;
+use sort_comp::unstable::rust_ipnsort as test_sort;
 
 fuzz_target!(|data: &[u8]| {
     let mut v = data.to_vec();
