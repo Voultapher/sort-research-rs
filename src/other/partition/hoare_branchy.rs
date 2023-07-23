d6127539273c0eab9a5fdbe763ea29921aadfa1a
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2023-07-23 14:48:19 +0200

    Fix bug in hoare_branchy and use swap_nonoverlapping

--- src/other/partition/hoare_branchy.rs
+++ src/other/partition/hoare_branchy.rs
@@ -7,24 +7,29 @@ fn partition<T, F>(v: &mut [T], pivot: &T, is_less: &mut F) -> usize
 where
     F: FnMut(&T, &T) -> bool,
 {
-    // Now partition the slice.
-    let mut l = 0;
-    let mut r = v.len();
-    loop {
-        // SAFETY: The unsafety below involves indexing an array.
-        // For the first one: We already do the bounds checking here with `l < r`.
-        // For the second one: We initially have `l == 0` and `r == v.len()` and we checked that `l < r` at every indexing operation.
-        //                     From here we know that `r` must be at least `r == l` which was shown to be valid from the first one.
-        unsafe {
+    // SAFETY: The unsafety below involves indexing an array. For the first one: We already do
+    // the bounds checking here with `l < r`. For the second one: We initially have `l == 0` and
+    // `r == v.len()` and we checked that `l < r` at every indexing operation.
+    //
+    // From here we know that `r` must be at least `r == l` which was shown to be valid from the
+    // first one.
+    unsafe {
+        let arr_ptr = v.as_mut_ptr();
+
+        let mut l = arr_ptr;
+        let mut r = arr_ptr.add(v.len());
+
+        loop {
             // Find the first element greater than the pivot.
-            while l < r && is_less(v.get_unchecked(l), pivot) {
-                l += 1;
+            while l < r && is_less(&*l, pivot) {
+                l = l.add(1);
             }
 
             // Find the last element equal to the pivot.
-            while l < r && !is_less(v.get_unchecked(r - 1), pivot) {
-                r -= 1;
+            while l < r && !is_less(&*r.sub(1), pivot) {
+                r = r.sub(1);
             }
+            r = r.sub(1);
 
             // Are we done?
             if l >= r {
@@ -32,13 +37,10 @@ where
             }
 
             // Swap the found pair of out-of-order elements.
-            r -= 1;
-            let ptr = v.as_mut_ptr();
-            ptr::swap(ptr.add(l), ptr.add(r));
-            l += 1;
+            ptr::swap_nonoverlapping(l, r, 1);
+            l = l.add(1);
         }
-    }
 
-    // We found `l` elements equal to the pivot. Add 1 to account for the pivot itself.
-    l + 1
+        l.sub_ptr(arr_ptr)
+    }
 }
