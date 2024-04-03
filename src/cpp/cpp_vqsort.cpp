a036779aa2d40187b1668d04a35953160de32dfa
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2024-04-03 16:01:32 +0200

    Remove misleading avx2 mention in vqsort
    It's been a while since the vendored version supports AVX-512 if available.

--- src/cpp/cpp_vqsort.cpp
+++ src/cpp/cpp_vqsort.cpp
@@ -9,80 +9,78 @@
 extern "C" {
 // --- i32 ---
 
-void vqsort_avx2_i32(int32_t* data, size_t len) {
+void vqsort_i32(int32_t* data, size_t len) {
   hwy::Sorter{}(data, len, hwy::SortAscending{});
 }
 
-uint32_t vqsort_avx2_i32_by(int32_t* data,
-                            size_t len,
-                            CompResult (*cmp_fn)(const int32_t&,
-                                                 const int32_t&,
-                                                 uint8_t*),
-                            uint8_t* ctx) {
+uint32_t vqsort_i32_by(int32_t* data,
+                       size_t len,
+                       CompResult (*cmp_fn)(const int32_t&,
+                                            const int32_t&,
+                                            uint8_t*),
+                       uint8_t* ctx) {
   printf("Not supported\n");
   return 1;
 }
 
 // --- u64 ---
 
-void vqsort_avx2_u64(uint64_t* data, size_t len) {
+void vqsort_u64(uint64_t* data, size_t len) {
   hwy::Sorter{}(data, len, hwy::SortAscending{});
 }
 
-uint32_t vqsort_avx2_u64_by(uint64_t* data,
-                            size_t len,
-                            CompResult (*cmp_fn)(const uint64_t&,
-                                                 const uint64_t&,
-                                                 uint8_t*),
-                            uint8_t* ctx) {
+uint32_t vqsort_u64_by(uint64_t* data,
+                       size_t len,
+                       CompResult (*cmp_fn)(const uint64_t&,
+                                            const uint64_t&,
+                                            uint8_t*),
+                       uint8_t* ctx) {
   printf("Not supported\n");
   return 1;
 }
 
 // --- ffi_string ---
 
-void vqsort_avx2_ffi_string(FFIString* data, size_t len) {
+void vqsort_ffi_string(FFIString* data, size_t len) {
   printf("Not supported\n");
 }
 
-uint32_t vqsort_avx2_ffi_string_by(FFIString* data,
-                                   size_t len,
-                                   CompResult (*cmp_fn)(const FFIString&,
-                                                        const FFIString&,
-                                                        uint8_t*),
-                                   uint8_t* ctx) {
+uint32_t vqsort_ffi_string_by(FFIString* data,
+                              size_t len,
+                              CompResult (*cmp_fn)(const FFIString&,
+                                                   const FFIString&,
+                                                   uint8_t*),
+                              uint8_t* ctx) {
   printf("Not supported\n");
   return 1;
 }
 
 // --- f128 ---
 
-void vqsort_avx2_f128(F128* data, size_t len) {
+void vqsort_f128(F128* data, size_t len) {
   printf("Not supported\n");
 }
 
-uint32_t vqsort_avx2_f128_by(F128* data,
-                             size_t len,
-                             CompResult (*cmp_fn)(const F128&,
-                                                  const F128&,
-                                                  uint8_t*),
-                             uint8_t* ctx) {
+uint32_t vqsort_f128_by(F128* data,
+                        size_t len,
+                        CompResult (*cmp_fn)(const F128&,
+                                             const F128&,
+                                             uint8_t*),
+                        uint8_t* ctx) {
   printf("Not supported\n");
   return 1;
 }
 
 // --- 1k ---
 
-void vqsort_avx2_1k(F128* data, size_t len) {
+void vqsort_1k(F128* data, size_t len) {
   printf("Not supported\n");
 }
 
-uint32_t vqsort_avx2_1k_by(F128* data,
-                           size_t len,
-                           CompResult (*cmp_fn)(const F128&,
-                                                const F128&,
-                                                uint8_t*),
-                           uint8_t* ctx) {
+uint32_t vqsort_1k_by(F128* data,
+                      size_t len,
+                      CompResult (*cmp_fn)(const F128&, const F128&, uint8_t*),
+                      uint8_t* ctx) {
   printf("Not supported\n");
   return 1;
 }
--- src/other/cpp_vqsort.rs
+++ src/other/cpp_vqsort.rs
@@ -1,1 +1,1 @@
-ffi_sort_impl!("cpp_vqsort", vqsort_avx2);
+ffi_sort_impl!("cpp_vqsort", vqsort);
