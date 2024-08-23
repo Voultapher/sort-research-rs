c600479b043f6476849fc6c75f46339ce7149220
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2024-08-23 11:54:53 +0200

    Dual test with driftsort

--- tests/main.rs
+++ tests/main.rs
@@ -1,5 +1,11 @@
-use sort_test_tools::instantiate_sort_tests;
+mod unstable {
+    type TestSort = sort_research_rs::unstable::rust_ipnsort::SortImpl;
 
-type TestSort = sort_research_rs::unstable::rust_ipnsort::SortImpl;
+    sort_test_tools::instantiate_sort_tests!(TestSort);
+}
 
-instantiate_sort_tests!(TestSort);
+mod stable {
+    type TestSort = sort_research_rs::stable::rust_driftsort::SortImpl;
+
+    sort_test_tools::instantiate_sort_tests!(TestSort);
+}
